b961e3113a79893f2e68625883d9ed6808101061
Danila Ermakov <danila.ermakov@gmail.com>
2019-12-17 20:34:06 -0500

    additional topic options implemented

--- src/main/kotlin/org/kafkalytic/plugin/CreateTopicDialog.kt
+++ src/main/kotlin/org/kafkalytic/plugin/CreateTopicDialog.kt
@@ -1,14 +1,16 @@
 package org.kafkalytic.plugin
 
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.ui.InputValidator
 import com.intellij.openapi.ui.Messages
+import com.intellij.ui.table.JBTable
 import java.awt.BorderLayout
-import java.awt.Color
 import java.awt.Dimension
 import java.awt.GridLayout
+import java.util.*
 import javax.swing.*
-import javax.swing.event.ChangeEvent
+import javax.swing.table.DefaultTableModel
 
 class CreateTopicDialog() : Messages.InputDialog(
         "Enter topic name",
@@ -23,6 +25,8 @@ class CreateTopicDialog() : Messages.InputDialog(
     private val LOG = Logger.getInstance(this::class.java)
     private lateinit var partitions: JTextField
     private lateinit var replication: JTextField
+    private lateinit var compression: ComboBox<String>
+    private lateinit var options: OptionsPanel
 
     override fun createMessagePanel(): JPanel {
         val messagePanel = JPanel(BorderLayout())
@@ -41,16 +45,87 @@ class CreateTopicDialog() : Messages.InputDialog(
         replication = JTextField("1")
         replication.preferredSize = Dimension(50, 24)
         replication.inputVerifier = INT_VERIFIER
-        val certPanel = JPanel(GridLayout(2, 2))
+
+        compression = ComboBox(KAFKA_COMPRESSION_TYPES)
+        options = OptionsPanel(arrayOf(
+                "cleanup.policy",
+                "delete.retention.ms",
+                "file.delete.delay.ms",
+                "flush.messages",
+                "flush.ms",
+                "follower.replication.throttled.replicas",
+                "index.interval.bytes",
+                "leader.replication.throttled.replicas",
+                "max.compaction.lag.ms",
+                "max.message.bytes",
+                "message.format.version",
+                "message.timestamp.difference.max.ms",
+                "message.timestamp.type",
+                "min.cleanable.dirty.ratio",
+                "min.compaction.lag.ms",
+                "min.insync.replicas",
+                "preallocate",
+                "retention.bytes",
+                "retention.ms",
+                "segment.bytes",
+                "segment.index.bytes",
+                "segment.jitter.ms",
+                "segment.ms",
+                "unclean.leader.election.enable",
+                "message.downconversion.enable"
+        ))
+
+        val certPanel = JPanel(GridLayout(0, 2))
+
         certPanel.addLabelled("Partitions", partitions)
         certPanel.addLabelled("Replication factor", replication)
+        certPanel.addLabelled("Compression", compression)
 
-        messagePanel.add(certPanel, BorderLayout.SOUTH)
+
+        messagePanel.add(layoutUD(certPanel, options), BorderLayout.SOUTH)
 
         return messagePanel
     }
 
+    class OptionsPanel(optionNames: Array<String>) : JPanel(BorderLayout()) {
+
+        private var tableModel: DefaultTableModel
+        private var options: ComboBox<String>
+        private var value: JTextField
+        init {
+            options = ComboBox(optionNames)
+            value = JTextField("")
+            tableModel = object: DefaultTableModel() {
+                override fun isCellEditable(row: Int, column: Int) = column == 1
+            }
+            tableModel.addColumn("Property")
+            tableModel.addColumn("Value")
+            tableModel.addTableModelListener {  }
+
+
+            val addButton = JButton("Add Property")
+            addButton.addActionListener {
+                if (value.text.isNotBlank()) {
+                    tableModel.addRow(arrayOf(options.selectedItem, value.text.trim()))
+                    options.removeItem(options.selectedItem)
+                } else {
+                    Messages.showInfoMessage("Property value cannot be blank", "Kafka")
+                }
+            }
+            add(addButton, BorderLayout.NORTH)
+            add(options, BorderLayout.WEST)
+            add(value, BorderLayout.CENTER)
+            add(JBTable(tableModel), BorderLayout.SOUTH)
+        }
+
+        fun getConfig() =
+            tableModel.dataVector.elements().asSequence()
+                    .map { val v = it as Vector<*>; v[0].toString() to v[1].toString() }.toMap()
+
+    }
+
     fun getTopic() = inputString!!
-    fun getPartitions() = partitions!!.text.toInt()
-    fun getReplications() = replication!!.text.toShort()
+    fun getPartitions() = partitions.text.toInt()
+    fun getReplications() = replication.text.toShort()
+    fun getConfig() = options.getConfig().toMutableMap().also {it.put("compression.type", "gzip")}
 }
